a2906f520e0b550c44c12bcdd6b9384ad5818052
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-11-29 13:33:36 -0500

    Refactor output logic

--- bin/pubquery.mjs
+++ bin/pubquery.mjs
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 
 const   str_aboutMe = `
 
@@ -7,17 +8,30 @@ NAME
 
 SYNOPSIS
 
-    node pmedquery.js
+    pmedquery.mjs   [--query <search>]              \\
+                    [--man]                         \\
+                    [--verbose <level>]             \\
+                    [--json] [--bibTeX] [--term]
 
 DESC
 
-    This simple nodejs program is an entrypoint that calls the pubmed_search
-    module to execute and parse pubmed queries.
+    This simple nodejs program restores CLI sanity to performing PubMed searches.
+    Rejoice!
 
 ARGS
 
-    --query <term>
-    The query term to send to PubMed.
+    --query <search>
+    The search term to send to PubMed. Any PubMed query construct is valid here.
+
+    --json
+    If specified, output the results as a JSON object.
+
+    --bibTeX
+    If specified, output the results in bibTeX format.
+
+    --term
+    If specified, output the results to the console/terminal. Note if neither
+    '--json' nor '--bibTex' is specified, then '--term' is assumed.
 
     --man
     Show this man page. All other options, even if specified, are ignored.
@@ -37,22 +51,6 @@ import { Refprint } from '../utils/refprint.mjs';
 const pubmed        = new PubMed();
 const output        = new outbox();
 output.outputBox_setup();
-
-function checkOn(payload) {
-    if(!payload.status) {
-        const error = `
-        ERROR:
-
-        In <${pubmed.activity}> the following occurred:
-        ${pubmed.errorMessage}
-
-        Returning to system.
-        `;
-        output.outputBox_print(error, 'error');
-        process.exit();
-    }
-    return payload.return;
-}
 
 const CLIoptions  = yargs(hideBin(process.argv))
     .options(
@@ -74,8 +72,61 @@ const CLIoptions  = yargs(hideBin(process.argv))
             type:           "boolean",
             default:        false
         })
+    .option(
+        "json", {
+            describe:       "If specified, output a JSON return",
+            type:           "boolean",
+            default:        false
+        })
+    .option(
+        "bibTeX", {
+            describe:       "If specified, output a bibTeX return",
+            type:           "boolean",
+            default:        false
+        })
+    .option(
+        "term", {
+            describe:       "If specified, output a terminal return",
+            type:           "boolean",
+            default:        false
+        })
+    .option(
+        "man", {
+            describe:       "If specified, show a man page",
+            type:           "boolean",
+            default:        false
+        })
     .parse();
 
+function checkOn(payload) {
+    if(!payload.status) {
+        const error = `
+        ERROR:
+
+        In <${pubmed.activity}> the following occurred:
+        ${pubmed.errorMessage}
+
+        Returning to system.
+        `;
+        output.outputBox_print(error, 'error');
+        process.exit();
+    }
+    return payload.return;
+}
+
+function output_process(publist) {
+    if(publist) {
+        if(!CLIoptions.json || !CLIoptions.bibTeX)
+            CLIoptions.term = true;
+        if(CLIoptions.term) {
+            const refprint    = new Refprint(publist);
+            refprint.do();
+        }
+        if(CLIoptions.json) {
+            console.log(JSON.stringify(publist, null, 4))
+        }
+    }
+}
 
 async function main() {
     // Process CLI flags
@@ -86,10 +137,7 @@ async function main() {
 
     const idlist  = checkOn(await pubmed.ids_get(CLIoptions.query));
     const publist = checkOn(await pubmed.ids_process(idlist));
-    if(publist) {
-        const refprint    = new Refprint(publist);
-        refprint.do();
-    }
+    output_process(publist);
 }
 
 main();
